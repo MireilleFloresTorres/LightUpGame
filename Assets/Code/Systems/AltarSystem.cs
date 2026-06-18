57033abbb353f8166e835670b20633eafc9b6cf8
MireilleFloresTorres <m.flores.wor@gmail.com>
2026-06-17 19:43:34 -0600

    Add Altar System
    System to recover the player's life little by little, public variables are taken from the player, an implementation to stop is missing - amount

---
+++ Assets/Code/Systems/AltarSystem.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+public class AltarSystem : MonoBehaviour
+{
+    [Header("Recarga")]
+    public float healPerTick = 5f;     // cuanto cura cada tick
+    public float tickInterval = 1f;    // cada cuáots segundos
+
+    //**IMPORTANTE: Es temporal
+    //realmente no recuerdo que tecla o si era con ratón que se activa el botón
+    //Así que es un tecla temporal
+    [Header("temporal")]
+    public KeyCode AlatarKey = KeyCode.R; 
+
+    private FlameHealth player;//vida del jugador
+    private bool isHealing = false;//Para saber si está curando o no 
+    private float tickTimer = 0f;//el timmer del tick, acumula 
+    private Vector3 lastPosition;//revisamos la posición del jugador
+
+    void Start()
+    {
+        player = FindObjectOfType<FlameHealth>();
+        lastPosition = player.transform.position;
+    }
+
+    void Update()
+    {
+
+        //**IMPORTANTE: Es tomporal
+        // aún no contamos con el sistema de detectar el radio de moviemiento
+        //la tecla es temporal y tendrá que actulizarse esta función
+        if (Input.GetKeyDown(AlatarKey))
+        {
+            isHealing = true;
+            tickTimer = 0f;
+        }
+
+        if (isHealing)
+        {
+            HealOverTime();
+        }
+    }
+
+    void HealOverTime()
+    {
+        tickTimer += Time.deltaTime;
+
+        if (tickTimer >= tickInterval)
+        {
+            tickTimer = 0f;
+            Activate();
+        }
+    }
+
+    public void Activate()
+    {
+        if (player == null) return;
+        player.Heal(healPerTick);
+    }
+}
+
+//**Falta implementar la parte de que detenga el ampunt que se resta solo para sumarlo**
